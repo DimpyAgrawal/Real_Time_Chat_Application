606a195f6d13b493a9dbcc81bc386a4b8188fff4
Dimpy Agrawal <dimpyagrawal101@gmail.com>
2024-04-15 15:50:51 +0530

    socket implemented

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
-// import ChatProvider from '../context/ChatProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // to access the all states in all over the wrap
-  // <ChatProvider>  
-  //       <App />
-  // </ChatProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
